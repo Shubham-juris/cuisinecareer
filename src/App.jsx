03ae9e1f53d1fd5bf77c1a892820a69e82a4a75a
It-juris <itjurisnexa@gmail.com>
2025-04-09 10:24:19 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 class='text-3xl font-bold underline text-center bg-stone-500 mt-12 py-12'>
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
